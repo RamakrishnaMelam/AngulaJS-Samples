af194745e9dc89447d899f95e7263f6559c4d66f
Ramakrishna <Ram.melam@gmail.com>
2016-08-29 02:53:06 +1000

    test

--- WebApplication1/WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/WebApplication1/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace AngularJS_Sample_Demo
+namespace WebApplication1
 {
     public class RouteConfig
     {
@@ -16,7 +16,7 @@ namespace AngularJS_Sample_Demo
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Product", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
